6c3dae8c0b1aef99bbe30d796797fdbcfdeb4794
thangtmc73 <thangtm.dev@gmail.com>
2021-09-19 10:42:20 +0700

    feat: handle user name in room page

--- client/src/containers/room-page/RoomPage.js
+++ client/src/containers/room-page/RoomPage.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function RoomPage() {
   const { roomId } = useParams();
-  return <div>Room {roomId}</div>;
+  const [nameSubmitded, setNameSubmitted] = useState(false);
+  const [name, setName] = useState("");
+
+  function handleNameChange(event) {
+    setName(event?.target?.value);
+  }
+
+  function handleSubmitClick() {
+    setNameSubmitted(true);
+  }
+
+  if (!nameSubmitded) {
+    return (
+      <div>
+        <h1>Room {roomId}</h1>
+        <input
+          placeholder="Type your name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <button onClick={handleSubmitClick}>Submit</button>
+      </div>
+    );
+  }
+  return <div>{name}</div>;
 }
 
 export default RoomPage;
