bd050ec74f356eec42f6eab9de72fba4e27a9953
thangtmc73 <thangtm.dev@gmail.com>
2021-09-19 10:52:36 +0700

    feat(client): add constant for list cards item

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,3 +2,34 @@ export const APP_PATH = {
   HomePage: "/",
   RoomPage: "/room/:roomId",
 };
+
+export const CARDS_VALUE = [
+  {
+    key: "one",
+    value: 1,
+  },
+  {
+    key: "two",
+    value: 2,
+  },
+  {
+    key: "three",
+    value: 3,
+  },
+  {
+    key: "five",
+    value: 5,
+  },
+  {
+    key: "eight",
+    value: 8,
+  },
+  {
+    key: "thirdteen",
+    value: 13,
+  },
+  {
+    key: "twenty-one",
+    value: 21,
+  },
+];
